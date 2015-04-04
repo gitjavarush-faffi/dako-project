2882235f51887e9739102811bff85ab1aefe95cc
gitjavarush-faffi <gitjavarush@gmail.com>
2015-04-04 19:50:23 +0300

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+
+  validates :title, :description, :image_url, :price, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+                          with: %r{\.(gif|png|jpeg|jpg)\Z}i,
+                          message: 'Invalid image url'
+                      }
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  def new_product(image_url)
+    Product.new(title: 'tmpTitle',
+                description: 'tmpDescription',
+                image_url: image_url,
+                price: 5)
+  end
+
+  test "everything is field" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: 'tmpTitle',
+                          description: 'tmpDescription',
+                          image_url: 'tmpImageUrl.png')
+    product.price = -1;
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+        product.errors[:price]
+
+    product.price = 0;
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+        product.errors[:price]
+
+    product.price = 1;
+    assert product.valid?
+  end
+
+  test "image url valid or invalid" do
+    ok = %w{tmp.png Tmp.png Tmp.Png tmp.jpg tmp.jpeg tmp.gif http://a.b.c/x/y/z/fred.gif}
+    bad = %w{tmp.doc tmp.rtp tmp.gif.more tmp.bat}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} souldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} souldn't be valid"
+    end
+  end
+
+  test "uniq title" do
+    product = Product.new(title: products(:yason).title,
+                          description: products(:yason).description,
+                          image_url: products(:yason).description,
+                          price: 9.99)
+    assert product.invalid?
+    assert_equal ['has already been taken'],
+        product.errors[:title]
+                                #assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+                                #product.errors[:title]
+  end
+
 end
